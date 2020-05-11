c41acf55e8fdb39a1d3e55baf99a96d3553d513c
Jeremy Curtis <jeremy.curtis808@gmail.com>
2020-05-11 16:14:32 -0500

    updating comments in script.js

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 
+
+//function to trigger sound corresponding to which key is pressed. Also applies .playing to pressed key and ensures audio always starts when pressed.
 const playSound = e => {
-    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-    const key = document.querySelector(`.key[data-key='${e.keyCode}']`)
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing')
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`); 
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`) 
+    if (!audio) return; 
+    audio.currentTime = 0; 
+    audio.play(); 
+    key.classList.add('playing') 
 }
 
-//--this way doesn't work because arrow function changes value of this to 'window' 
+//--this way doesn't work because arrow function changes value of 'this' to 'window' 
 // const removeTransition = e => {
 //     if (e.propertyName !== 'transform') return;
 //     console.log(this);
 // }
 
+
+//function to remove .playing from key pressed
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+    if (e.propertyName !== 'transform') return; 
+    this.classList.remove('playing'); 
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+//adding event listener to all keys to remove .playing after transition
+const keys = document.querySelectorAll('.key'); 
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); 
 
 window.addEventListener('keydown', playSound);
 
